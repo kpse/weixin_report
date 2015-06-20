a83e5eface3e6dd670810727873a631505c96c7f
Suo Qin <kpsesuo@gmail.com>
2015-06-20 17:40:39 +0800

    wechat-crypto

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var WXBizMsgCrypt = require('wechat-crypto');
+
+var config = {
+  token: 'LQbOWl94FP8E',
+  encodingAESKey: '878BkiLfb2H3nAV28TBYOLykt5EnVrec2PujHElTyWR',
+  corpId: 'wx0328f4ebaec4d5d4'
+};
+
+router.get('/', function(req, res){
+  var msg_signature = req.query.msg_signature;
+  var timestamp = req.query.timestamp;
+  var nonce = req.query.nonce;
+  var echostr = req.query.echostr;
+  var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId)
+  var s = cryptor.decrypt(echostr);
+  res.send(s.message);
 });
 
 module.exports = router;
